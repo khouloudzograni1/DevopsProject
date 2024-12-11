d90e19053f000a14b01bbdbaab7f00293dc622a5
khouloudzograni1 <khouloud.zograni@esprit.tn>
2024-12-11 02:45:20 +0100

    update

--- src/main/java/tn/esprit/eventsproject/services/EventServicesImpl.java
+++ src/main/java/tn/esprit/eventsproject/services/EventServicesImpl.java
@@ -26,47 +26,43 @@ public class EventServicesImpl implements IEventServices{
     public Participant addParticipant(Participant participant) {
         return participantRepository.save(participant);
     }
+
     @Override
     public List<Participant> getAllParticipants() {
         return participantRepository.findAll() ;
     }
+
     @Override
     public Event addAffectEvenParticipant(Event event, int idParticipant) {
-        // Récupérer le participant, s'il existe
         Participant participant = participantRepository.findById(idParticipant).orElse(null);
-        if (participant == null) {
-            throw new IllegalArgumentException("Participant with ID " + idParticipant + " does not exist.");
-        }
-        if (participant.getEvents() == null) {
+        if(participant.getEvents() == null){
             Set<Event> events = new HashSet<>();
             events.add(event);
             participant.setEvents(events);
-        } else {
+        }else {
             participant.getEvents().add(event);
         }
         return eventRepository.save(event);
     }
+
     @Override
     public Event addAffectEvenParticipant(Event event) {
-        // Récupérer les participants associés à l'événement
         Set<Participant> participants = event.getParticipants();
-        for (Participant aParticipant : participants) {
-            // Récupérer le participant existant par son ID
+        for(Participant aParticipant:participants){
             Participant participant = participantRepository.findById(aParticipant.getIdPart()).orElse(null);
-            if (participant == null) {
-                throw new IllegalArgumentException("Participant with ID " + aParticipant.getIdPart() + " does not exist.");
-            }
-            if (participant.getEvents() == null) {
+            if(participant.getEvents() == null){
                 Set<Event> events = new HashSet<>();
                 events.add(event);
                 participant.setEvents(events);
-            } else {
+            }else {
                 participant.getEvents().add(event);
             }
-            participantRepository.save(participant);
         }
         return eventRepository.save(event);
     }
+
+
+
     @Override
     public Logistics addAffectLog(Logistics logistics, String descriptionEvent) {
       Event event = eventRepository.findByDescription(descriptionEvent);
@@ -81,15 +77,18 @@ public class EventServicesImpl implements IEventServices{
       }
         return logisticsRepository.save(logistics);
     }
+
     @Override
     public List<Logistics> getLogisticsDates(LocalDate dateDebut, LocalDate dateFin) {
         List<Event> events = eventRepository.findByDateDebutBetween(dateDebut, dateFin);
+
         List<Logistics> logisticsList = new ArrayList<>();
         for (Event event:events){
             if(event.getLogistics().isEmpty()){
 
                 return Collections.emptyList();
             }
+
             else {
                 Set<Logistics> logisticsSet = event.getLogistics();
                 for (Logistics logistics:logisticsSet){
